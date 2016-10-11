f694ae19ce3a413ceeb21d2e9987d95fd480e9a9
Snaedis Kjartansdóttir <snaedis@Snaediss-MacBook-Pro-2.local>
2016-10-11 18:29:29 +0000

    fixing files

---
+++ src/test/java/stringCalculator/kataCalculatorTest.java
@@ -0,0 +1,27 @@
+public class kataCalculatorTest {
+
+	public static void main(String args[]) {
+      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+    }
+
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, Calculator.add(""));
+	}
+
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}
+
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, Calculator.add("1,2"));
+	}	
+
+	@Test
+    public void testMultipleNumbers(){
+    	assertEquals(6, Calculator.add("1,2,3"));
+    }
+
+}
