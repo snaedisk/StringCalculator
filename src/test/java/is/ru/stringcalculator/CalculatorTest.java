d188e0be61627c0d9a1f651cb1c02735a6fdb9fc
Snaedis Kjartansdóttir <snaedis@Snaediss-MacBook-Pro-2.local>
2016-10-12 23:11:17 +0000

    testing Multiple numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -15,4 +15,14 @@ public class CalculatorTest {
 		assertEquals(1, Calculator.add("1"));
 	}
 
+
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, Calculator.add("1,2"));
+	}
+
+	@Test
+    public void testMultipleNumbers(){
+    	assertEquals(6, Calculator.add("1,2,3"));
+    }
 }
