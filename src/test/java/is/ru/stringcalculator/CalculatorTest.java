ce918a65d2785ce4fd2d2f85a8fbfae2c32e1af2
Snaedis Kjartansdóttir <snaedis@Snaediss-MacBook-Pro-2.local>
2016-10-13 11:35:17 +0000

    testing new lines between numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -31,4 +31,9 @@ public class CalculatorTest {
         assertEquals(3+6+15+18+46+33, Calculator.add("3,6,15,18,46,33"));
     }
 
+    @Test
+    public void testNewLines() {
+        assertEquals(6, Calculator.add("1\n2,3"));
+    }
+
 }
